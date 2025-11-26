9d563028acfd32fd2fbd114f1e0f46b8d6a6bf28
akash.keshari <akash.keshari@hasanbhaikifaujkeraygimoujo.onmicrosoft.com>
2025-11-26 14:02:18 +0530

    update server

--- modules/azurerm_mssql_server/data.tf
+++ modules/azurerm_mssql_server/data.tf
@@ -1,14 +1,14 @@
-data "azurerm_key_vault" "kv" {
-  name                = "todokey"
-  resource_group_name = "rg-test"
-}
+# data "azurerm_key_vault" "kv" {
+#   name                = "todokey"
+#   resource_group_name = "rg-test"
+# }
 
-data "azurerm_key_vault_secret" "username" {
-  name         = "username"
-  key_vault_id = data.azurerm_key_vault.kv.id
-}
+# data "azurerm_key_vault_secret" "username" {
+#   name         = "username"
+#   key_vault_id = data.azurerm_key_vault.kv.id
+# }
 
-data "azurerm_key_vault_secret" "password" {
-  name         = "password"
-  key_vault_id = data.azurerm_key_vault.kv.id
-}
+# data "azurerm_key_vault_secret" "password" {
+#   name         = "password"
+#   key_vault_id = data.azurerm_key_vault.kv.id
+# }
--- modules/azurerm_mssql_server/main.tf
+++ modules/azurerm_mssql_server/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_mssql_server" "mssql-server" {
   resource_group_name          = each.value.resource_group_name
   location                     = each.value.location
   version                      = each.value.version                   
-  administrator_login          = data.azurerm_key_vault_secret.username.value
-  administrator_login_password = data.azurerm_key_vault_secret.password.value
+  administrator_login          = "serveradmin"
+  administrator_login_password = "ServerPassword"
   minimum_tls_version          = each.value.minimum_tls_version                                     #"1.2"
 }
